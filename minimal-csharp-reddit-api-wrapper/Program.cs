2f0901ff6efb351a395aa08ebf6d448d26bf3cf9
Damian Kowalik <dev.damian.kowalik@gmail.com>
2020-05-08 16:03:58 +0200

    Add Reddit class to streamline experience

--- minimal-csharp-reddit-api-wrapper/Program.cs
+++ minimal-csharp-reddit-api-wrapper/Program.cs
@@ -12,29 +12,14 @@ namespace MinimalRedditWrapper
 {
     internal class Program
     {
+        const string APP_ID = "g1b0mLRvuF7Sfg";
         private static async Task Main(string[] args)
         {
-            var token = await Token.Create("g1b0mLRvuF7Sfg");
-
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("Authorization", "bearer " + token.Value);
-            client.DefaultRequestHeaders.Add("User-Agent", "reddit-api-tests by st_aeon");
-
-            var response = await client.GetAsync("https://oauth.reddit.com/r/wallpapers/.json");
-            var responseString = await response.Content.ReadAsStringAsync();
-            var responseObject = JsonConvert.DeserializeObject(responseString);
-            Console.WriteLine(responseObject);
-            var listing = JsonConvert.DeserializeObject<Listing<Post>>(responseString);
-
-            Console.WriteLine("AFTER: {0}", listing.Data.After);
-            var afterResponse = await client.GetAsync("https://oauth.reddit.com/r/wallpapers?after="+listing.Data.After);
-            var afterResponseString = await afterResponse.Content.ReadAsStringAsync();
-            var afterResponseObject = JsonConvert.DeserializeObject(afterResponseString);
-            // Console.WriteLine(afterResponseObject);
-            var afterListing = JsonConvert.DeserializeObject<Listing<Post>>(afterResponseString);
+            var reddit = await Reddit.Create(APP_ID);
+            var listing = await reddit.GetSubreddit("wallpapers");
 
 
-            foreach (var child in afterListing.Data.Children)
+            foreach (var child in listing.Data.Children)
             {
                 Console.WriteLine(child.Data.Title);
                 Console.WriteLine(child.Data.Url);
---
+++ minimal-csharp-reddit-api-wrapper/Reddit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MinimalRedditWrapper.Things
+{
+    public class Reddit
+    {
+        private Token _token;
+        private HttpClient _client;
+        public string RequestUrl => "https://oauth.reddit.com/";
+        public string SubredditRequestUrl => RequestUrl + "r/";
+        public static async Task<Reddit>  Create(string appId)
+        {
+            Reddit res = new Reddit
+            {
+                _token =  await Token.Create(appId),
+                _client = new HttpClient()
+            };
+
+            res._client.DefaultRequestHeaders.Add("Authorization", "bearer " + res._token.Value);
+            res._client.DefaultRequestHeaders.Add("User-Agent", "reddit-api-tests by st_aeon");
+
+            return res;
+        }
+
+        public async Task<Listing<Post>> GetSubreddit(string name)
+        {
+            var response = await _client.GetAsync(SubredditRequestUrl + "wallpapers");
+            var responseString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Listing<Post>>(responseString);
+        }
+    }
+}
